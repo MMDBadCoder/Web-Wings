e43c38860837e46aa8ea44211bfb78182239e62b
mohammad <gitmmdgit@gmail.com>
2024-10-25 04:27:59 +0330

    add migrate before execute

--- core-server/scripts/migrate-and-run-server.sh
+++ core-server/scripts/migrate-and-run-server.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sleep 5
+echo "Sleep 10 seconds to be sure that database is up!"
+sleep 10
+echo "Try to execute migrations..."
 alembic -c alembic/alembic.ini upgrade head
+echo "Running the application"
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload
