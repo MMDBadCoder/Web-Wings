0064744e1c1dea0d5938e054714ce3c5057f8208
mohammad <gitmmdgit@gmail.com>
2024-11-10 03:18:37 +0330

    make commands

--- core-server/scripts/migrate-and-run-server.sh
+++ core-server/scripts/migrate-and-run-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+echo "Sleep 10 seconds to be sure that database is up!"
+sleep 10
 echo "Try to execute migrations..."
 alembic -c alembic/alembic.ini upgrade head
 echo "Running the application"
-docker run -d --name web-wings-core --network=host web-wings:0.2
-
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload
