c3f321221096ed67fe764e2c19e31d261c2fd66f
mohammad <gitmmdgit@gmail.com>
2024-11-10 03:16:00 +0330

    make commands

--- core-server/scripts/migrate-and-run-server.sh
+++ core-server/scripts/migrate-and-run-server.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "Sleep 10 seconds to be sure that database is up!"
-sleep 10
 echo "Try to execute migrations..."
 alembic -c alembic/alembic.ini upgrade head
 echo "Running the application"
