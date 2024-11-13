b0aeda92732af668205b315ce401dbcf69b16d97
mohammad <gitmmdgit@gmail.com>
2024-11-14 01:49:28 +0330

    make paymon of irancell snapp tapsi

--- paymon/models.py
+++ paymon/models.py
@@ -2,3 +2,9 @@ class DataPoint:
     def __init__(self, timestamp: int, value: int):
         self.timestamp = timestamp
         self.value = value
+
+
+class RetrievedSession:
+    def __init__(self, headers, cookies):
+        self.headers = headers
+        self.cookies = cookies
